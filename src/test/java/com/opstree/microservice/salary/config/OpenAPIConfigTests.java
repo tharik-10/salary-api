6715388340fbb201f8abac8cd940f7b6378f4543
tharik-10 <md.tharik@mygurukulam.co>
2025-05-29 16:31:12 +0530

    Update OpenAPIConfigTests.java

--- src/test/java/com/opstree/microservice/salary/config/OpenAPIConfigTests.java
+++ src/test/java/com/opstree/microservice/salary/config/OpenAPIConfigTests.java
@@ -26,7 +26,7 @@ class OpenAPIConfigTest {
         // Verify the server details
         assertEquals(1, openAPI.getServers().size());
         Server server = openAPI.getServers().get(0);
-        assertEquals("http://localhost:8080", server.getUrl());
+        assertEquals("http://34.238.122.10:8081", server.getUrl());
         assertEquals("Server URL in Development environment", server.getDescription());
 
         // Verify the contact details
